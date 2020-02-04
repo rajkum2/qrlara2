30e19a763c013bc6a32adc4fb867899330eb261c
kiran <kumar.alg2@gmail.com>
2020-02-04 14:46:49 +0530

    adding QRcode model and layouts

---
+++ app/Models/Qrcode.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @SWG\Definition(
+ *      definition="Qrcode",
+ *      required={""},
+ *      @SWG\Property(
+ *          property="id",
+ *          description="id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="user_id",
+ *          description="user_id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="website",
+ *          description="website",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="product_name",
+ *          description="product_name",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="product_url",
+ *          description="product_url",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="company_name",
+ *          description="company_name",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="callback_url",
+ *          description="callback_url",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="qrcode_path",
+ *          description="qrcode_path",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="amount",
+ *          description="amount",
+ *          type="number",
+ *          format="number"
+ *      ),
+ *      @SWG\Property(
+ *          property="status",
+ *          description="status",
+ *          type="boolean"
+ *      ),
+ *      @SWG\Property(
+ *          property="created_at",
+ *          description="created_at",
+ *          type="string",
+ *          format="date-time"
+ *      ),
+ *      @SWG\Property(
+ *          property="updated_at",
+ *          description="updated_at",
+ *          type="string",
+ *          format="date-time"
+ *      )
+ * )
+ */
+class Qrcode extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'qrcodes';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+    protected $dates = ['deleted_at'];
+
+
+
+    public $fillable = [
+        'user_id',
+        'website',
+        'product_name',
+        'product_url',
+        'company_name',
+        'callback_url',
+        'qrcode_path',
+        'amount',
+        'status'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'user_id' => 'integer',
+        'website' => 'string',
+        'product_name' => 'string',
+        'product_url' => 'string',
+        'company_name' => 'string',
+        'callback_url' => 'string',
+        'qrcode_path' => 'integer',
+        'amount' => 'float',
+        'status' => 'boolean'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'user_id' => 'required',
+        'product_name' => 'required',
+        'company_name' => 'required',
+        'callback_url' => 'required',
+        'amount' => 'required',
+        'status' => 'required'
+    ];
+
+    
+}
---
+++ app/Repositories/QrcodeRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Qrcode;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class QrcodeRepository
+ * @package App\Repositories
+ * @version February 4, 2020, 9:16 am UTC
+*/
+
+class QrcodeRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'user_id',
+        'website',
+        'product_name',
+        'product_url',
+        'company_name',
+        'callback_url',
+        'qrcode_path',
+        'amount',
+        'status'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Qrcode::class;
+    }
+}
